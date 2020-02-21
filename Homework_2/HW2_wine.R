a08a91cbe0505a297c60a206eb01166400d218fe
Cedric Kerley <kerleyc@wit.edu>
2020-02-21 10:21:26 -0500

    Quick fix

--- Homework_2/HW2_wine.R
+++ Homework_2/HW2_wine.R
@@ -1,8 +1,8 @@
 #build a decision tree using rpart
 library(rpart)
-winetest <- read.csv("winedata.csv")
+winetrain <- read.csv("winedata.csv")
 #shamelessly stolen from the rpart_in_lession script
-cfit <- rpart(as.factor(c)~.,data=winetest,method="class")
+cfit <- rpart(as.factor(c)~.,data=winetrain,method="class")
 name1="decisionTREE_wine"
 num=1
 ext=".pdf"
@@ -12,23 +12,6 @@ plot(cfit,uniform=T,main="Decision Tree for Wine data")
 text(cfit,use.n=T,all=T,cex=.6)
 dev.off()
 pred <- predict(cfit,data=winetest,type="class")
-t <- table(winetest$c,pred)
+t <- table(winetrain$c,pred)
 print(t)
-stop()
-d <- diag(t)
-n <- sum(t)
-overall_ac=d/n
-n1 <- sum(t[1,])
-ns <- t[1,1]
-Acc1=ns/n1
-print(Acc1)
-n2 <- sum(t[2,])
-ns <- t[2,2]
-print(ns/n2)
-#regression
-rfit <- rpart(Sepal.Length ~ Sepal.Width + Petal.Length + 
-                Petal.Width, method="anova",data=iris)
-plot(rfit,uniform=T,main="regression for iris")
-text(rfit,use.n=T,cex=.6)
-p <- predict(rfit,iris,method="anova")
-print(p)
+#run test data through decision tree
